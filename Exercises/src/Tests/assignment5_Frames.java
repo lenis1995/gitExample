0561136c6fb7cb2616a22499320e4e4eb9109dfc
lenis <lenisalejo@gmail.com>
2020-06-28 17:44:36 -0500

    is just a message

--- Exercises/src/Tests/assignment5_Frames.java
+++ Exercises/src/Tests/assignment5_Frames.java
@@ -12,11 +12,11 @@ public class assignment5_Frames {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int in=0;
-		String s="felicidad,paciencia,persistencia";
-		char c='d';
-		boolean b=false;
+		System.out.println("I'm finding a new job");
+		int in = 0;
+		String s = "felicidad,paciencia,persistencia";
+		char c = 'd';
+		boolean b = false;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenix\\Desktop\\CURSOS PROGRAMACIÓN\\CURSO SELENIUM "
 				+ "CON JAVA\\CURSO SELENIUM UDEMY\\DRIVERS\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver;
@@ -24,21 +24,22 @@ public class assignment5_Frames {
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/nested_frames");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		WebDriverWait wait=new WebDriverWait(driver,15);
-		
-		//	driver.findElement(By.xpath("//a[text()='Nested Frames']")).click();
-		WebElement topFrame=driver.findElement(By.xpath("//frame[@name='frame-top']"));
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+
+		// driver.findElement(By.xpath("//a[text()='Nested Frames']")).click();
+		WebElement topFrame = driver.findElement(By.xpath("//frame[@name='frame-top']"));
 		driver.switchTo().frame(topFrame);
-		
-		WebElement middleFrame=	driver.findElement(By.xpath("//frame[@name='frame-middle']"));
-		driver.switchTo().frame(middleFrame);	
-		
-		String frameMessage=driver.findElement(By.cssSelector("div#content")).getText();
+
+		WebElement middleFrame = driver.findElement(By.xpath("//frame[@name='frame-middle']"));
+		driver.switchTo().frame(middleFrame);
+
+		String frameMessage = driver.findElement(By.cssSelector("div#content")).getText();
 		System.out.println(frameMessage);
-		
+
 		driver.switchTo().parentFrame();
-		//driver.findElement(By.cssSelector("div#content")).getText(); No lo puede encontrar ya que esta en el frame padre.
-		
+		// driver.findElement(By.cssSelector("div#content")).getText(); No lo puede
+		// encontrar ya que esta en el frame padre.
+
 		driver.quit();
 	}
 
